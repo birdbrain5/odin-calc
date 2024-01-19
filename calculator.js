b7c0a2446585dc7b7668a3de618c8d29f2328ffa
birdbrain5 <nicholaspingas@gmail.com>
2024-01-19 05:42:46 -0500

    created basic page layout & stylings

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const btns = document.getElementsByClassName("calcBtn")
+var add = document.getElementById("add")
+var subtract = document.getElementById("subtract")
+var multiply = document.getElementById("multiply")
+var divide = document.getElementById("divide")
+
+function displayInput(){
+    
+}
